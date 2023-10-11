808f47d0accac744ff0fd1b4b1154ca9bdff5909
vitor <vitor@DESKTOP-20MT7U4>
2023-10-11 16:33:36 -0300

    Adicionar arquivos de projeto.

---
+++ AlphaSystemDesktop/formConsulta.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace AlphaSystemDesktop
+{
+    partial class formConsulta
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formConsulta));
+            this.panelMenu = new System.Windows.Forms.FlowLayoutPanel();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.btnConsultas = new System.Windows.Forms.Button();
+            this.panelTitleBar = new System.Windows.Forms.Panel();
+            this.panelFormulario = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateCompetencia = new System.Windows.Forms.DateTimePicker();
+            this.txtFuncionario = new System.Windows.Forms.TextBox();
+            this.lblCompetencia = new System.Windows.Forms.Label();
+            this.lblTipoDeDemonstrativo = new System.Windows.Forms.Label();
+            this.lblFuncionario = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panelMenu.SuspendLayout();
+            this.panelFormulario.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(74)))), ((int)(((byte)(98)))));
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Controls.Add(this.btnInicio);
+            this.panelMenu.Controls.Add(this.btnConsultas);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(134, 517);
+            this.panelMenu.TabIndex = 0;
+            this.panelMenu.Paint += new System.Windows.Forms.PaintEventHandler(this.panelMenu_Paint);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackgroundImage = global::AlphaSystemDesktop.Properties.Resources.v5;
+            this.panelLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(3, 3);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(134, 80);
+            this.panelLogo.TabIndex = 0;
+            this.panelLogo.Paint += new System.Windows.Forms.PaintEventHandler(this.panelLogo_Paint);
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnInicio.FlatAppearance.BorderSize = 0;
+            this.btnInicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInicio.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnInicio.Image = global::AlphaSystemDesktop.Properties.Resources.house_32;
+            this.btnInicio.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnInicio.Location = new System.Drawing.Point(3, 89);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnInicio.Size = new System.Drawing.Size(134, 50);
+            this.btnInicio.TabIndex = 1;
+            this.btnInicio.Text = "  Inicio";
+            this.btnInicio.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnInicio.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnInicio.UseVisualStyleBackColor = true;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // btnConsultas
+            // 
+            this.btnConsultas.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnConsultas.FlatAppearance.BorderSize = 0;
+            this.btnConsultas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConsultas.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnConsultas.Image = ((System.Drawing.Image)(resources.GetObject("btnConsultas.Image")));
+            this.btnConsultas.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnConsultas.Location = new System.Drawing.Point(3, 145);
+            this.btnConsultas.Name = "btnConsultas";
+            this.btnConsultas.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnConsultas.Size = new System.Drawing.Size(134, 50);
+            this.btnConsultas.TabIndex = 2;
+            this.btnConsultas.Text = "  Consultas";
+            this.btnConsultas.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnConsultas.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnConsultas.UseVisualStyleBackColor = true;
+            // 
+            // panelTitleBar
+            // 
+            this.panelTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(74)))), ((int)(((byte)(98)))));
+            this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitleBar.Location = new System.Drawing.Point(134, 0);
+            this.panelTitleBar.Name = "panelTitleBar";
+            this.panelTitleBar.Size = new System.Drawing.Size(1134, 50);
+            this.panelTitleBar.TabIndex = 1;
+            // 
+            // panelFormulario
+            // 
+            this.panelFormulario.Controls.Add(this.comboBox1);
+            this.panelFormulario.Controls.Add(this.button1);
+            this.panelFormulario.Controls.Add(this.dateCompetencia);
+            this.panelFormulario.Controls.Add(this.txtFuncionario);
+            this.panelFormulario.Controls.Add(this.lblCompetencia);
+            this.panelFormulario.Controls.Add(this.lblTipoDeDemonstrativo);
+            this.panelFormulario.Controls.Add(this.lblFuncionario);
+            this.panelFormulario.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelFormulario.Location = new System.Drawing.Point(134, 50);
+            this.panelFormulario.Name = "panelFormulario";
+            this.panelFormulario.Size = new System.Drawing.Size(1134, 352);
+            this.panelFormulario.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkOrange;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(645, 310);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 30);
+            this.button1.TabIndex = 15;
+            this.button1.Text = " Consultar";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // dateCompetencia
+            // 
+            this.dateCompetencia.Location = new System.Drawing.Point(394, 246);
+            this.dateCompetencia.Name = "dateCompetencia";
+            this.dateCompetencia.Size = new System.Drawing.Size(295, 20);
+            this.dateCompetencia.TabIndex = 14;
+            // 
+            // txtFuncionario
+            // 
+            this.txtFuncionario.Location = new System.Drawing.Point(394, 108);
+            this.txtFuncionario.Name = "txtFuncionario";
+            this.txtFuncionario.Size = new System.Drawing.Size(650, 20);
+            this.txtFuncionario.TabIndex = 12;
+            // 
+            // lblCompetencia
+            // 
+            this.lblCompetencia.AutoSize = true;
+            this.lblCompetencia.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCompetencia.Location = new System.Drawing.Point(221, 246);
+            this.lblCompetencia.Name = "lblCompetencia";
+            this.lblCompetencia.Size = new System.Drawing.Size(116, 19);
+            this.lblCompetencia.TabIndex = 11;
+            this.lblCompetencia.Text = "Competência:";
+            // 
+            // lblTipoDeDemonstrativo
+            // 
+            this.lblTipoDeDemonstrativo.AutoSize = true;
+            this.lblTipoDeDemonstrativo.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipoDeDemonstrativo.Location = new System.Drawing.Point(151, 176);
+            this.lblTipoDeDemonstrativo.Name = "lblTipoDeDemonstrativo";
+            this.lblTipoDeDemonstrativo.Size = new System.Drawing.Size(186, 19);
+            this.lblTipoDeDemonstrativo.TabIndex = 10;
+            this.lblTipoDeDemonstrativo.Text = "Tipo de demonstrativo:";
+            // 
+            // lblFuncionario
+            // 
+            this.lblFuncionario.AutoSize = true;
+            this.lblFuncionario.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFuncionario.Location = new System.Drawing.Point(230, 108);
+            this.lblFuncionario.Name = "lblFuncionario";
+            this.lblFuncionario.Size = new System.Drawing.Size(107, 19);
+            this.lblFuncionario.TabIndex = 9;
+            this.lblFuncionario.Text = "Funcionario:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(394, 174);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(650, 21);
+            this.comboBox1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1268, 517);
+            this.Controls.Add(this.panelFormulario);
+            this.Controls.Add(this.panelTitleBar);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelMenu.ResumeLayout(false);
+            this.panelFormulario.ResumeLayout(false);
+            this.panelFormulario.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel panelMenu;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Button btnConsultas;
+        private System.Windows.Forms.Panel panelTitleBar;
+        private System.Windows.Forms.Panel panelFormulario;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateCompetencia;
+        private System.Windows.Forms.TextBox txtFuncionario;
+        private System.Windows.Forms.Label lblCompetencia;
+        private System.Windows.Forms.Label lblTipoDeDemonstrativo;
+        private System.Windows.Forms.Label lblFuncionario;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ AlphaSystemDesktop/formConsulta.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlphaSystemDesktop
+{
+    public partial class formConsulta : Form
+    {
+        public formConsulta()
+        {
+            InitializeComponent();
+        }
+
+        private void panelMenu_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelLogo_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
